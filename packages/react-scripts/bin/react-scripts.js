4012fcf8d0c70bbae3e26beb1a293ae9de8cb0a9
Dan Proudfoot <dan@kerve.co.uk>
2018-05-09 12:04:16 +0100

    Alias to scoped scripts

--- packages/react-scripts/bin/react-scripts.js
+++ packages/react-scripts/bin/react-scripts.js
@@ -8,6 +8,8 @@
 
 'use strict';
 
+require('module-alias/register');
+
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
